b4ae0e2d4bd2d0f37fdddf79e91b1f992eb9fbcf
muhammadsyazili <09021181520135@students.ilkom.unsri.ac.id>
2022-03-08 14:10:56 +0700

    080322

--- app/Services/AnalyticValidationService.php
+++ app/Services/AnalyticValidationService.php
@@ -5,8 +5,8 @@ namespace App\Services;
 use App\DTO\ConstructRequest;
 use App\Repositories\IndicatorRepository;
 use App\Repositories\UserRepository;
-use App\Rules\Level__IsThisAndChildFromUser__Except__Employee;
-use App\Rules\Unit__IsThisAndChildUser__Except__Employee;
+use App\Rules\Level__IsThisAndChildFromUser__Custom;
+use App\Rules\Unit__IsThisAndChildUser__Custom;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
@@ -33,8 +33,8 @@ class AnalyticValidationService
         $user = $this->userRepository->find__with__role_unit_level__by__id($request->header('X-User-Id'));
 
         $attributes = [
-            'level' => ['required', 'string', 'not_in:super-master'], //new Level__IsThisAndChildFromUser__Except__Employee($user)
-            'unit' => ['required', 'string', 'not_in:master'], //new Unit__IsThisAndChildUser__Except__Employee($user)
+            'level' => ['required', 'string', 'not_in:super-master', new Level__IsThisAndChildFromUser__Custom($user)],
+            'unit' => ['required', 'string', 'not_in:master', new Unit__IsThisAndChildUser__Custom($user)],
             'tahun' => ['required', 'integer', 'date_format:Y'],
             'bulan' => ['required', 'string', 'in:jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec'],
         ];
